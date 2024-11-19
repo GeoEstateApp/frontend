889317b749f36e7d908f170208e7e33f3c4f59d9
Ayesha Virk <ashiasad546@gmail.com>
2024-11-19 22:06:59 +0500

    updated

--- app/index.tsx
+++ app/index.tsx
@@ -16,15 +16,15 @@ import TeamDetail from "./landing-team-details/TeamDetail";
 import DemoVideo from './landing-demo-video/DemoVideo';
 
 const teamMembers = [
-    { name: "Ayesha Virk", role: "Founder, Full Stack Developer", image: "https://i.pinimg.com/736x/9f/90/0f/9f900f10d2ffdfd34c95ae39487eff85.jpg" },
+    { name: "Ayesha Virk", role: "Founder, Full Stack Developer", image: "https://i.pinimg.com/736x/0a/7d/c3/0a7dc35b8155aba1008c563bbbe34441.jpg" },
     { name: "Robert Bui", role: "CEO, Backend Lead", image: "https://i.pinimg.com/736x/c6/dd/02/c6dd02eef170fb8accaf180faf0fdea8.jpg" },
-    { name: "Humera", role: "Backend Developer, DB Developer", image: "https://i.pinimg.com/736x/0a/7d/c3/0a7dc35b8155aba1008c563bbbe34441.jpg" },
-    { name: "Yaseen (Ace)", role: "Backend Developer", image: "https://i.pinimg.com/736x/d6/66/88/d66688f7a1f37ba4f0448d62876e028f.jpg" },
-    { name: "Aditya Sengupta", role: "Frontend Lead, Full Stack Developer", image: "https://i.pinimg.com/736x/a0/9a/e4/a09ae493347c98b507b43e8e2bed85be.jpg" },
+    { name: "Humera", role: "Backend Developer, DB Developer", image: "https://i.pinimg.com/736x/9f/90/0f/9f900f10d2ffdfd34c95ae39487eff85.jpg" },
+    { name: "Yaseen", role: "Backend Developer", image: "https://i.pinimg.com/736x/d6/66/88/d66688f7a1f37ba4f0448d62876e028f.jpg" },
+    { name: "Aditya Sengupta", role: "Frontend, Full Stack Developer", image: "https://i.pinimg.com/736x/a0/9a/e4/a09ae493347c98b507b43e8e2bed85be.jpg" },
     { name: "Dip", role: "Backend, Full Stack Developer", image: "https://i.pinimg.com/736x/e2/89/34/e2893485506bbf68ea7ac95c1dbb4bcd.jpg" },
     { name: "Zainab Rashid", role: "Frontend Developer, UI/UX Lead", image: "https://i.pinimg.com/736x/af/c5/57/afc557911778f8935553c46f5898dd39.jpg" },
     { name: "Shelian Gladis", role: "Frontend Developer, Designer", image: "https://i.pinimg.com/474x/ff/1c/d9/ff1cd938ef943a6af3b58ec33d39a81a.jpg" },
-    { name: "Vikramaditya Dhumal", role: "Frontend Developer", image: "https://i.pinimg.com/736x/79/36/6a/79366a3b921eb0c283e19b28edc80e61.jpg" },
+    { name: "Vikramaditya Dhumal", role: "Frontend Lead, Full Stack Developer", image: "https://i.pinimg.com/736x/79/36/6a/79366a3b921eb0c283e19b28edc80e61.jpg" },
 ];
 
 
